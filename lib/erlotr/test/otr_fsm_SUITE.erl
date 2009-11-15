cbddfa298c46e7081b23664a770fc6e7eff1e6da
Stefan Grundmann <sg2342@googlemail.com>
2009-11-15 23:33:25 +0100

    fix testcases, add one testcase, that does AKE and message xfer

--- lib/erlotr/test/otr_fsm_SUITE.erl
+++ lib/erlotr/test/otr_fsm_SUITE.erl
@@ -10,13 +10,28 @@
 
 -include("MessageTestVectors.hrl").
 
-init_per_suite(Config) -> Config.
+-include("DSATestVectors.hrl").
 
-end_per_suite(Config) -> Config.
+init_per_suite(Config) ->
+    case application:start(crypto) of
+      ok ->
+	  ct:comment("crypto application started"),
+	  [{stop_crypto, true} | Config];
+      {error, {already_started, crypto}} -> Config
+    end.
+
+end_per_suite(Config) ->
+    case proplists:lookup(stop_crypto, Config) of
+      {stop_crypto, true} ->
+	  application:stop(crypto),
+	  ct:comment("crypto application stopped");
+      _ -> ok
+    end,
+    Config.
 
 all() ->
     [pt_usr_1, pt_usr_2, pt_usr_start, pt_usr_stop,
-     pt_net_1, pt_net_error_1, pt_net_error_2, pt_net_data,
+     pt_net_1, pt_net_error_1, pt_net_error_2, pt_net_data, ake_msg,
      cover].
 
 %F{{{ init_per_testcase/2
@@ -24,12 +39,21 @@ init_per_testcase(pt_usr_2, Config) ->
     setup_fsm(Config, [send_whitespace_tag]);
 init_per_testcase(pt_net_error_2, Config) ->
     setup_fsm(Config, [error_start_ake]);
+init_per_testcase(ake_msg, Config) -> setup_pair(Config);
 init_per_testcase(_TestCase, Config) ->
     setup_fsm(Config, []).
 
 %}}}F
 
 %F{{{ end_per_testcase/2
+end_per_testcase(ake_msg, Config) ->
+    catch unlink(whereis(fsm2)),
+    catch exit(whereis(fsm2), shutdown),
+    catch unlink(whereis(fsm1)),
+    catch exit(whereis(fsm1), shutdown),
+    catch unlink(whereis(net_pipe)),
+    net_pipe ! stop,
+    Config;
 end_per_testcase(_TestCase, Config) ->
     case ?config(fsm, Config) of
       undefined -> ok;
@@ -46,6 +70,18 @@ end_per_testcase(_TestCase, Config) ->
 	  false = is_process_alive(Parser)
     end,
     Config.%}}}F
+
+ake_msg(_Config) ->
+    otr_fsm:consume(fsm1, {user, start_otr}),
+    {to_user1, {info, {encrypted_new_dsa_fp, _, _}}} =
+	receive X -> erlang:display(X), X end,
+    {to_user2, {info, {encrypted_new_dsa_fp, _, _}}} =
+	receive Y -> erlang:display(Y), Y end,
+    otr_fsm:consume(fsm2, {user, {message, "FOOO"}}),
+    {to_user1, {message, "FOOO"}} = receive
+				      Z -> erlang:display(Z), Z
+				    end,
+    ok.
 
 pt_usr_1(Config) ->
     ct:comment("message from user while in state [plaintext]"),
@@ -131,13 +167,45 @@ cover(_Config) ->
     ok.
 
 %F{{{ internal functions
+
+setup_pair(Config) ->
+    Self = self(),
+    EmitUser1 = fun (X) -> Self ! {to_user1, X} end,
+    EmitUser2 = fun (X) -> Self ! {to_user2, X} end,
+    EmitNet = fun (X) -> net_pipe ! {self(), X} end,
+    {ok, Parser1} = otr_parser_fsm:start_link(),
+    {ok, Parser2} = otr_parser_fsm:start_link(),
+    {ok, Fsm1} = otr_fsm:start_link([{emit_user, EmitUser1},
+				     {emit_net, EmitNet}, {dsa, ?DSAKey1}]),
+    {ok, Fsm2} = otr_fsm:start_link([{emit_user, EmitUser2},
+				     {emit_net, EmitNet}, {dsa, ?DSAKey2}]),
+    otr_parser_fsm:set_emit_fun(Parser1,
+				fun (X) -> otr_fsm:consume(Fsm1, {net, X}) end),
+    otr_parser_fsm:set_emit_fun(Parser2,
+				fun (X) -> otr_fsm:consume(Fsm2, {net, X}) end),
+    NetPipe = spawn_link(fun () ->
+				 net_pipe(Fsm1, Parser1, Fsm2, Parser2)
+			 end),
+    register(net_pipe, NetPipe),
+    register(fsm1, Fsm1),
+    register(fsm2, Fsm2),
+    Config.
+
+net_pipe(Fsm1, Parser1, Fsm2, Parser2) ->
+    receive
+      {Fsm1, M} -> otr_parser_fsm:consume(Parser2, M);
+      {Fsm2, M} -> otr_parser_fsm:consume(Parser1, M);
+      _ -> exit(kill)
+    end,
+    net_pipe(Fsm1, Parser1, Fsm2, Parser2).
+
 setup_fsm(Config, Opts) ->
     Self = self(),
     EmitUser = fun (X) -> Self ! {to_user, X} end,
     EmitNet = fun (X) -> Self ! {to_net, X} end,
     {ok, Parser} = otr_parser_fsm:start_link(),
     {ok, Fsm} = otr_fsm:start_link([{emit_user, EmitUser},
-				    {emit_net, EmitNet}
+				    {emit_net, EmitNet}, {dsa, ?DSAKey1}
 				    | Opts]),
     otr_parser_fsm:set_emit_fun(Parser,
 				fun (X) -> otr_fsm:consume(Fsm, {net, X}) end),
